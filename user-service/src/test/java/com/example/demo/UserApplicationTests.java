d63e5852a9d8d5eb85acec561829e9d3914f68bd
Itsjeet007y1 <jeet.62.jp@gmail.com>
2023-08-30 11:18:42 +0530

    Updated to UserApplicationTests

--- user-service/src/test/java/com/example/demo/UserApplicationTests.java
+++ user-service/src/test/java/com/example/demo/UserApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DamcoAssessmentApplicationTests {
+class UserApplicationTests {
 
 	@Test
 	void contextLoads() {
